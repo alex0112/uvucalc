5c148bb00bb2d33a90fd42b988922b42c0821b42
chasepants <chase.parks94@gmail.com>
2018-02-01 21:12:19 -0700

    changed main.js to controller.js so we can have a controller.js and a model.js where we can hold an array for the input screen

---
+++ controller.js
@@ -0,0 +1,14 @@
+// This is the javascript!  Yup.
+
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+
+function updateScreen() {
+    
+}
+
+function enter() {}
